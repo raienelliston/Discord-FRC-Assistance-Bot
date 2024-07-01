cfe50201aee797d8544f54f132d5f83c37497c67
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-01 08:23:17 -0700

    added discord connection (need to test)

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
+const express = require('express');
+const app = express();
 const { Client, GatewayIntentBits } = require('discord.js');
+const { InteractionType, InteractionResponseType, InteractionResponseFlags, MessageComponentTypes, ButtonStyleTypes } = require('discord-interactions');
+const { VerifyDiscordRequest } = require('./APIs/discordApi');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildMessages
-    ] 
+app.post('/interactions', async (req, res) => {
+    const { type } = req.body;
+
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+      }
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
-});
-
-client.login(process.env.DISCORD_BOT_TOKEN);
